14d02ac71d124096c876860ece7202df60e8d88c
Kevin Addeman <kevin.addeman@gmail.com>
2024-03-31 21:37:39 -0400

    Add submode 0x09 to map

--- components/cn105/Globals.h
+++ components/cn105/Globals.h
@@ -89,8 +89,8 @@ static const char* TIMER_MODE_MAP[4] = { "NONE", "OFF", "ON", "BOTH" };
 static const uint8_t STAGE[7] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
 static const char* STAGE_MAP[7] = { "IDLE", "LOW", "GENTLE", "MEDIUM", "MODERATE", "HIGH", "DIFFUSE" };
 
-static const uint8_t SUB_MODE[4] = { 0x00, 0x02, 0x04, 0x08 };
-static const char* SUB_MODE_MAP[4] = { "NORMAL", "DEFROST", "PREHEAT", "STANDBY" };
+static const uint8_t SUB_MODE[4] = { 0x00, 0x02, 0x04, 0x08, 0x09 };
+static const char* SUB_MODE_MAP[4] = { "NORMAL", "DEFROST", "PREHEAT", "STANDBY", "STANDBY-09" };
 static const uint8_t AUTO_SUB_MODE[4] = { 0x00, 0x01, 0x02, 0x03 };
 static const char* AUTO_SUB_MODE_MAP[4] = { "AUTO_OFF","AUTO_COOL", "AUTO_HEAT", "AUTO_LEADER" };
 
